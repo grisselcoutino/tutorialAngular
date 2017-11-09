4db428bef9d219d4a486a194c15ce4f47766a0a8
Grissel <grissel.coutino@experimenta.com.mx>
2017-11-09 08:55:32 -0600

    Revert "messaage servicio error"
    This reverts commit 552dc1620453eb812cb672e1d914d384751635c7.

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { HeroService } from './hero.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -11,14 +10,13 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent,
-    HeroService
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [ HeroService, MessageService ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
